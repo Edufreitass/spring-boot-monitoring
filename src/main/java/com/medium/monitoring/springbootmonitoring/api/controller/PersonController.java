b5c6126a0a14f0dd2fe148e5625d86403b8a0040
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-05-23 23:18:03 -0300

    Implements controller class

---
+++ src/main/java/com/medium/monitoring/springbootmonitoring/api/controller/PersonController.java
@@ -0,0 +1,11 @@
+package com.medium.monitoring.springbootmonitoring.api.controller;
+
+import com.medium.monitoring.springbootmonitoring.infrastructure.repository.PersonRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+}
